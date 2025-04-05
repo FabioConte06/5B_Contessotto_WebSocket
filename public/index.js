8515451210bea66cec7d79a6e4d4dd392e5c2838
Fabio <contessottofabio@itis-molinari.eu>
2025-04-05 02:29:03 +0200

    client

--- public/index.js
+++ public/index.js
@@ -1,37 +1,72 @@
 const input = document.getElementById("input");
 const button = document.getElementById("sendButton");
 const chat = document.getElementById("chat");
+const userListElement = document.getElementById("userList");
+const setNameButton = document.getElementById("setNameButton");
+const usernameInput = document.getElementById("username");
 
 const template = "<li class=\"list-group-item\">%MESSAGE</li>";
 const messages = [];
+const users = [];
 
 const socket = io();
 
-input.onkeydown = (event) => {
-  
-  if (event.keyCode === 13) {
-      event.preventDefault();
-      button.click();
+window.onload = () => {
+  $('#nameModal').modal('show');
+};
+
+setNameButton.onclick = () => {
+  const username = usernameInput.value.trim();
+  if (username) {
+    socket.emit("setName", username);
+    $('#nameModal').modal('hide');
   }
-}
+};
+
+input.onkeydown = (event) => {
+  if (event.keyCode === 13) {
+    event.preventDefault();
+    button.click();
+  }
+};
 
 button.onclick = () => {
   socket.emit("message", input.value);
   input.value = "";
-}
+};
 
+// Ricezione messaggi di chat
 socket.on("chat", (message) => {
   console.log(message);
   messages.push(message);
-  render();
-})
+  renderChat();
+});
 
-const render = () => {
+// Ricezione lista utenti
+socket.on("list", (list) => {
+  users.length = 0; // Pulisci l'array degli utenti
+  list.forEach(user => {
+    users.push(user.name);
+  });
+  renderUserList();
+});
+
+// Funzione per rendere la chat
+const renderChat = () => {
   let html = "";
   messages.forEach((message) => {
     const row = template.replace("%MESSAGE", message);
-    html+=row;
+    html += row;
   });
   chat.innerHTML = html;
   window.scrollTo(0, document.body.scrollHeight);
-}
+};
+
+// Funzione per rendere la lista degli utenti
+const renderUserList = () => {
+  userListElement.innerHTML = ""; // Pulisci la lista esistente
+  users.forEach((user) => {
+    const row = `<li class="list-group-item">${user}</li>`;
+    userListElement.innerHTML += row;
+  });
+};
